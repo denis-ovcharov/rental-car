849fffd998ff01b32e8d736bc1e48207a843c160
Denys Ovcharov <denisovcharov88@gmail.com>
2026-05-15 00:11:50 +0200

    styles for home

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,11 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <div
-      className="w-full h-[400px] md:h-[550px] lg:h-[700px] bg-cover bg-center flex flex-col items-center justify-end pb-[40px] md:pb-[60px]"
-      style={{ backgroundImage: "url('/home-bg.jpg')" }}
+      className="w-full h-[400px] md:h-[550px] lg:h-[700px] bg-cover bg-center flex flex-col items-center justify-end pb-[40px] md:pb-[60px] h-screen"
+      style={{
+        backgroundImage: "url('/home-bg.jpg')",
+        height: 'calc(100vh - 68px)',
+      }}
     >
       <div className="text-center text-white flex flex-col items-center gap-4 px-4 justify-center md:justify-end">
         <h1 className="font-bold text-[32px] md:text-[48px] lg:text-[60px] leading-[120%]">
