5fec12e4cb067616bbdb1fd0d491d54e6ac455c1
Denys Ovcharov <denisovcharov88@gmail.com>
2026-05-19 22:08:04 +0200

    fix header styles

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -6,31 +6,34 @@ import { usePathname } from 'next/navigation';
 export default function Header() {
   const pathname = usePathname();
   return (
-    <header className="container flex justify-between items-center h-17 py-6 overflow-hidden bg-white">
-      <Link href={'/'}>
-        <svg width="104" height="16" aria-hidden="true">
-          <use href="/sprite.svg#logo" />
-        </svg>
-      </Link>
-      <nav className="flex gap-4">
-        {' '}
-        <Link
-          href={'/'}
-          className={
-            pathname === '/' ? 'text-button' : 'hover:text-button-hover'
-          }
-        >
-          Home
+    <header className="w-full overflow-hidden bg-white">
+      <div className="mx-auto flex h-17 w-full max-w-360 items-center justify-between px-5 py-6 md:px-8 2xl:px-30">
+        <Link href={'/'}>
+          <svg width="104" height="16" aria-hidden="true">
+            <use href="/sprite.svg#logo" />
+          </svg>
         </Link>
-        <Link
-          href={'/catalog'}
-          className={
-            pathname === '/catalog' ? 'text-button' : 'hover:text-button-hover'
-          }
-        >
-          Catalog
-        </Link>
-      </nav>
+        <nav className="flex gap-4">
+          <Link
+            href={'/'}
+            className={
+              pathname === '/' ? 'text-button' : 'hover:text-button-hover'
+            }
+          >
+            Home
+          </Link>
+          <Link
+            href={'/catalog'}
+            className={
+              pathname === '/catalog'
+                ? 'text-button'
+                : 'hover:text-button-hover'
+            }
+          >
+            Catalog
+          </Link>
+        </nav>
+      </div>
     </header>
   );
 }
