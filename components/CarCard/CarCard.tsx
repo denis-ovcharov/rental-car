1309efec4e4ede5a68dd543c0a6f16bf67f02ef4
Denys Ovcharov <denisovcharov88@gmail.com>
2026-05-20 11:09:18 +0200

    fix car card styles

--- components/CarCard/CarCard.tsx
+++ components/CarCard/CarCard.tsx
@@ -31,16 +31,16 @@ export default function CarCard({ car }: CarCardProps) {
         <p className="font-semibold text-base">${car.rentalPrice}</p>
       </div>
 
-      <ul className="flex flex-wrap gap-x-2 gap-y-1 text-xs text-main flex-1 mb-3">
-        <li>{city}</li>
-        <li>|</li>
-        <li>{country}</li>
-        <li>|</li>
-        <li>{car.rentalCompany}</li>
-        <li>|</li>
-        <li>{car.type}</li>
-        <li>|</li>
-        <li>{car.mileage.toLocaleString()} km</li>
+      <ul className="flex flex-wrap  gap-y-1 text-xs text-main flex-1 mb-3 items-center">
+        <li className="border-r border-gray-light px-2">{city}</li>
+
+        <li className="border-r border-gray-light px-2">{country}</li>
+
+        <li className="border-r border-gray-light px-2">{car.rentalCompany}</li>
+
+        <li className="border-r border-gray-light px-2">{car.type}</li>
+
+        <li className="px-2">{car.mileage.toLocaleString()} km</li>
       </ul>
 
       <Button
